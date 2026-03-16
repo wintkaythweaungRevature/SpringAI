eca7e9f297352c50ebda8c9077c1b9aaf167ab10
Wint Kay <ms.wintkaythweaung@gmail.com>
2026-03-15 22:35:10 -0400

    try to conenct SN accounts

--- backend/src/main/java/com/wintaibot/entity/User.java
+++ backend/src/main/java/com/wintaibot/entity/User.java
@@ -14,7 +14,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
     @Column(name = "first_name")
